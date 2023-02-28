944bda7fa108e45857e6967e4607c714d8f2dcc5
Yiddies <chrislicks104@gmail.com>
2023-02-27 19:16:17 -0500

    The basic programs
    thinking i will change it to push the number into a array rather than
    hardcoding a num1 and num2, so that you can add more than 2 numbers
    
    yeah, ill probably do that. but my head hurts now

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
-const calcScreen = document.querySelector('.screen')
+let currentOperator = ''
 
-const numBtns = document.querySelectorAll(".numButton")
+let numBtns = document.querySelectorAll(".numButton")
+let operBtns = document.querySelectorAll(".operButton")
+let calcScreen = document.querySelector('.screen')
+
+
+operBtns.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+);
+
 
 numBtns.forEach((button) =>
     button.addEventListener('click', () => appendNumber(button.textContent))
-)
+);
+
 function appendNumber(number) {
-    console.log(number)
+
+    if (currentOperator != '') {
+        calcScreen.innerHTML += number
+        let num2 = calcScreen.innerHTML;
+        console.log("num2 " + num2)
+        return num2;
+    } else {
+        calcScreen.innerHTML += number
+        let num1 = calcScreen.innerHTML;
+        console.log("num1 " + num1)
+        return num1;
+    }
+}
+
+function setOperation(id) {
+
+    calcScreen.innerHTML = ''
+    if (id === '+') {
+
+        console.log(id)
+        return currentOperator = 'add'
+    } else if (id === '-') {
+        console.log(id)
+        return currentOperator = 'subtract'
+    } else if (id === '*') {
+        console.log(id)
+        return currentOperator = 'multiply'
+    } else {
+        console.log(id)
+        return currentOperator = 'division'
+    }
 }
