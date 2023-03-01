3d936dc81a6cefdc56b4d0f30ba92778b1d1a3e3
Yiddies <chrislicks104@gmail.com>
2023-03-01 17:33:04 -0500

    added array for numbers

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let currentOperator = ''
+let nums = []
+let numbers = ''
 
 let numBtns = document.querySelectorAll(".numButton")
 let operBtns = document.querySelectorAll(".operButton")
@@ -15,35 +17,34 @@ numBtns.forEach((button) =>
 );
 
 function appendNumber(number) {
-
-    if (currentOperator != '') {
-        calcScreen.innerHTML += number
-        let num2 = calcScreen.innerHTML;
-        console.log("num2 " + num2)
-        return num2;
-    } else {
-        calcScreen.innerHTML += number
-        let num1 = calcScreen.innerHTML;
-        console.log("num1 " + num1)
-        return num1;
-    }
+    calcScreen.innerHTML += number
+    numbers += number
+    console.log(numbers)
+    return numbers;
 }
-
 function setOperation(id) {
 
     calcScreen.innerHTML = ''
     if (id === '+') {
-
         console.log(id)
+        nums.push(numbers)
+        console.log(nums)
+        numbers = ''
         return currentOperator = 'add'
     } else if (id === '-') {
+        nums.push(numbers)
         console.log(id)
+        numbers = ''
         return currentOperator = 'subtract'
     } else if (id === '*') {
+        nums.push(numbers)
         console.log(id)
+        numbers = ''
         return currentOperator = 'multiply'
     } else {
+        nums.push(numbers)
         console.log(id)
+        numbers = ''
         return currentOperator = 'division'
     }
 }
