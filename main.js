0bccc73bc5af888b483383ff23077a229e717c4e
Yiddies <chrislicks104@gmail.com>
2023-03-01 18:32:33 -0500

    got the subtraction working

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let currentOperator = ''
 const nums = []
 let numbers = ''
-let sum = 0
+let sum = ''
+let newSum = ''
 
 let numBtns = document.querySelectorAll(".numButton")
 let operBtns = document.querySelectorAll(".operButton")
@@ -40,16 +41,19 @@ function setOperation(id) {
     } else if (id === '-') {
         nums.push(Number(numbers))
         console.log(id)
+        console.log(nums)
         numbers = ''
         return currentOperator = 'subtract'
     } else if (id === '*') {
         nums.push(Number(numbers))
         console.log(id)
+        console.log(nums)
         numbers = ''
         return currentOperator = 'multiply'
     } else {
         nums.push(Number(numbers))
         console.log(id)
+        console.log(nums)
         numbers = ''
         return currentOperator = 'division'
     }
@@ -62,10 +66,20 @@ function operate(array) {
         });
         console.log(sum)
     } else if (currentOperator === 'subtract') {
-
+        sum = array[0]
+        for (let i = 1; i < array.length; i++) {
+            sum -= array[i];
+        }
+        console.log(sum)
     } else if (currentOperator === 'multiply') {
-
+        array.forEach(item => {
+            sum *= item;
+        });
+        console.log(sum)
     } else {
-
+        array.forEach(item => {
+            sum /= item;
+        });
+        console.log(sum)
     }
 }
